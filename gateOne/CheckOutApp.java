431340c8222eaae2fdf293043ad21e65a51987ed
olawhales <ajaditaoreed@gmail.com>
2024-08-06 00:20:04 +0100

    program in progress

--- gateOne/CheckOutApp.java
+++ gateOne/CheckOutApp.java
@@ -4,27 +4,30 @@ public class CheckOutApp{
 	   Scanner input = new Scanner(System.in);
 
 
-		int total = 0 ;
-		int customer = 0;
 		boolean user = true;
 		int sum = 0;
+		String item = "";
+		int quantity = 0;
+		int price = 0;
+		int discount = 0;
 
 		System.out.print("whats the customer's Names: ");
 		String customerName = input.nextLine();
 
 	do{
 		System.out.print("whats did the user buy? ");
-		String item = input.nextLine();
+		item = input.nextLine();
 
 		System.out.print("How many pieces? ");
-		int quantity = input.nextInt();
+		quantity = input.nextInt();
 
 		System.out.print("How much per Unit: ");
-		int price = input.nextInt();
+		price = input.nextInt();
 		sum += price ;
 		
 		System.out.print("How much discount will he got? ");
-		int discount = input.nextInt();
+		int discounts = input.nextInt();
+		discount = discounts / 100 * sum ;
 
 
 		System.out.println("Continue (yes/no)");
@@ -35,14 +38,20 @@ public class CheckOutApp{
 		System.out.println("Thanks for your patronage!!!");
 
 		input.close();
+
 		System.out.println("==============================================================");
 		System.out.printf("%s\n%s\n%s\n%s\n%s\n%s","SEMICOLON", "MAIN BRANCH", "LOCATION: 312, HERBERT MACAULEY WAY, SABO YABA, LAGOS.","TEL: 08102790000","Date: 5th of August 2024","Cashier: Cashier's Name");
 		System.out.println("Customer Name: " + customerName );
 		System.out.println("==============================================================");
 	System.out.println("		ITEM	QTY	PRICE	TOTAL(NGN)");
 	System.out.println("--------------------------------------------------------------");
-	System.out.print(item);
-	//System.out.printf("%4d%4d%4d", quantity, price, sum);
+	System.out.printf("		%4s%6d%9d%9d",item, quantity, price, sum);
+	System.out.println();
+	System.out.println("--------------------------------------------------------------");
+	System.out.printf("		%s%4d\n		%s%4d", "Sub Total:	", sum, "Discount:	", discount );
+
+
+
 	
 	
 	
