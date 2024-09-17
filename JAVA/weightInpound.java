379fe355ed908113b923dc3774d94fe708b328a6
olawhales <ajaditaoreed@gmail.com>
2024-09-17 18:39:21 +0100

    change

--- JAVA/weightInpound.java
+++ JAVA/weightInpound.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class weightInpound{
-	public static void main( String [] args){
+	public static void main( String ... args){
 	 	Scanner input = new Scanner(System.in);
 
 		System.out.print("Enter your weight in kilogram: ");
